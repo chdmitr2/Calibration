cfbdf0d9daacf503d832129489ef6486399033ad
chdmitr2@gmail.com <chdmitr2@gmail.com>
2023-05-18 05:32:14 +0300

    Add project files.

---
+++ Calibration/Algorithm.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using _Excel = Microsoft.Office.Interop.Excel;
+
+
+
+namespace Calibration
+{
+    public class Algorithm
+    {
+        public List<int> SubBand { get; }
+        public List<double> TunedFrequency { get; }
+        public List<double> NotchRejection { get; }
+
+        Excel excel = new Excel("C:\\Users\\chdmi\\OneDrive\\Desktop\\Full_Table2.xlsx", 1);
+        Excel excelBest = new Excel("C:\\Users\\chdmi\\OneDrive\\Desktop\\BestNotch.xlsx", 1);
+
+        public Algorithm()
+        {
+            int i = 1;
+            string subband = "subband";
+            List<int> SubBand = new List<int>();
+            OpenFile();
+            while(subband != string.Empty)
+            {
+                subband = excel.ReadCell(i, 3);
+                if (subband == string.Empty) continue;
+                else
+                {
+                    excelBest.WriteToCell(i, 3, subband);
+
+
+                   // excel.SaveAs("C:\\Users\\chdmi\\OneDrive\\Desktop\\BestNotch.xlsx");
+                   // excelBest.Save();
+                  
+                  
+               
+                   // excel.Close();
+                    i++;
+                    int numOfSubband = Int32.Parse(subband);
+                    SubBand.Add(numOfSubband);
+                }
+                
+            }
+        }
+
+        public Algorithm(List<int> subBand, List<double> tunedFrequency, List<double> notchRejection)
+        {
+
+        }
+
+       
+
+        public void OpenFile()
+        {
+            
+           // MessageBox.Show(excel.ReadCell(1, 3));
+           // excel.Close();
+
+        }
+    }
+}
---
+++ Calibration/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calibration
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Algorithm StartCalibration = new Algorithm();
+            //OpenFile();
+            //WriteData();
+        }
+
+       
+
+      //  public void WriteData()
+        //{
+        //    Excel excel = new Excel("C:\\Users\\chdmi\\OneDrive\\Desktop\\BestNotch.xlsx", 1);
+        //    excel.WriteToCell(0, 0, "Test2");
+         //   excel.Save();
+         //   excel.SaveAs("C:\\Users\\chdmi\\OneDrive\\Desktop\\BestNotch.xlsx");
+         //   excel.Close();
+       // }
+        
+    }
+}
